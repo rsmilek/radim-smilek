9dfd3af4d62ff192436b1ee89739e0c801dce49c
rsmilek <smilek.radim@gmail.com>
2020-06-25 20:56:51 +0200

    * orignal Delog navigation replaced by my own navBar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
-import Navigation from "../components/navigation";
+// import { Link, useStaticQuery, graphql } from "gatsby";
+// import Navigation from "../components/navigation";
 import "prismjs/themes/prism-okaidia.css";
 
 export default ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  );
+  // const data = useStaticQuery(
+  //   graphql`
+  //     query {
+  //       site {
+  //         siteMetadata {
+  //           title
+  //         }
+  //       }
+  //     }
+  //   `
+  // );
   return (
     <div className="site-wrapper">
       <header className="site-header">
-        <div className="site-title">
+        {/* <div className="site-title">
           <Link to="/">{data.site.siteMetadata.title}</Link>
-        </div>
-        <Navigation />
+        </div> */}
+        {/* <Navigation /> */}
       </header>
       {children}
       <footer className="site-footer">
