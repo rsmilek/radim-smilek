d9f0835c6c7dd55542fce9caf877c5977678c02c
rsmilek <smilek.radim@gmail.com>
2020-07-01 15:37:18 +0200

    * refactoring of mainNavbar.scss to be imported into global.scss

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,6 @@
+// Styles
 import "bootstrap/dist/css/bootstrap.min.css";
-// Styles for DELOG
 import "./src/styles/global.scss";
-
-// Styles for NAVBAR
-//import "bootstrap/dist/css/bootstrap.min.css";
-import "./src/styles/mainNavbar.scss"; // Core theme CSS (includes Bootstrap)
-
 // JS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap core JS
 import "jquery/dist/jquery.min.js";
