7c78a379b0926244648f30d0dba3ee95c153417d
rsmilek <smilek.radim@gmail.com>
2020-07-10 20:04:38 +0200

    + added Gatsby & Netlify icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import IconGatsby from "../../static/assets/skills/gatsby.svg";
+import IconNetlify from "../../static/assets/skills/netlify.svg";
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="container">
-        <footer className="footer">
-          <p>Copyright &copy; {new Date().getFullYear()} &bull; Designed &amp; developed by Radim Smílek</p>
-        </footer>
-      </div>
+      <footer className="footer">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} &bull; Designed &amp; developed by Radim Smílek with{" "}
+          <a href="https://www.gatsbyjs.org/">
+            Gatsby
+            {<IconGatsby className="footer-icon" />}
+          </a>{" "}
+          &amp;{" "}
+          <a href="https://www.netlify.com/">
+            Netlify
+            {<IconNetlify className="footer-icon" />}
+          </a>
+        </p>
+      </footer>
     );
   }
 }
