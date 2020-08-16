7443cb0301a9898b7db8fa76ff455dfefdee229f
rsmilek <smilek.radim@gmail.com>
2020-08-16 16:32:40 +0200

    * just refactoring to modern use element refs

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,57 +1,36 @@
-import React, { Component, createRef, RefObject } from "react";
+import React, { Component } from "react";
 import gsap from "gsap";
 
-type TDivRef = RefObject<HTMLDivElement> | null;
 type TDiv = HTMLDivElement | null;
 
 export default class Home extends Component<{}> {
-  headlines: TDivRef = createRef();
-  headlineFirst: TDivRef = createRef();
-  headlineSecond: TDivRef = createRef();
-  headlineThird: TDivRef = createRef();
-  divider: TDivRef = createRef();
-  description: TDivRef = createRef();
+  headlineElementFirst: TDiv = null;
+  headlineElementSecond: TDiv = null;
+  headlineElementThird: TDiv = null;
+  dividerElement: TDiv = null;
+  descriptionElement: TDiv = null;
   animationTween = gsap.timeline({ paused: true });
 
   componentDidMount() {
     this.animationTween
+      // Headline - moving
       .from(
-        [this.headlineFirst!.current, this.headlineSecond!.current, this.headlineThird!.current],
+        [this.headlineElementFirst, this.headlineElementSecond, this.headlineElementThird],
         { y: 44, stagger: 0.1, ease: "none" },
         0.2
       )
+      // Headline - visibility
       .from(
-        [this.headlineFirst!.current, this.headlineSecond!.current, this.headlineThird!.current],
+        [this.headlineElementFirst, this.headlineElementSecond, this.headlineElementThird],
         { opacity: 0, stagger: 0.1, ease: "power2.in" },
         0.2
       )
-      .from(this.divider!.current, { opacity: 0, ease: "none", duration: 1 }, 0.3)
-      .from(this.description!.current, { opacity: 0, x: -200, ease: "elastic.out", duration: 2 }, 0.3);
-    this.animationTween.play();
+      // Divider - visibility
+      .from(this.dividerElement, { opacity: 0, ease: "none", duration: 1 }, 0.3)
+      // Description - visibility
+      .from(this.descriptionElement, { opacity: 0, x: -200, ease: "elastic.out", duration: 2 }, 0.3)
+      .play();
   }
-  //   this.animationTween.from(
-  //     [this.headlineFirst!.current, this.headlineSecond!.current, this.headlineThird!.current],
-  //     { y: 44, opacity: 0, stagger: 0.25, ease: "none" },
-  //     0.75
-  //   );
-
-  //
-  //   this.animationTween.from(
-  //     [this.headlineFirst!.current, this.headlineSecond!.current, this.headlineThird!.current],
-  //     { y: 44, opacity: 0, stagger: 1, ease: "power3.out" },
-  //     0.25
-  //   );
-
-  //   { y: 44, ease: Power3.easeOut, delay: 0.8, stagger: 0.15 },
-  //   { opacity: 0, stagger: 0.15, ease: "elastic" },
-  //   { opacity: 0, stagger: 0.15, ease: "power3.out" },
-  //   { y: 44, opacity: 0, stagger: 0.25, ease: "power3.out" },
-  //
-  //   .to(
-  //     titleElementsCurrent,
-  //     { duration: DURATION, scale: 1, opacity: 1, delay: 0, stagger: STAGGER, ease: "elastic", force3D: true },
-  //     0 + OFFSET
-  //   )
 
   render() {
     return (
@@ -59,21 +38,23 @@ export default class Home extends Component<{}> {
         <div className="container h-100">
           <div className="row h-100 align-items-center justify-content-center text-center">
             <div className="col-lg-10 align-self-end">
-              <div ref={this.headlines}>
-                <h1 className="text-black font-weight-bold" ref={this.headlineFirst}>
+              <div>
+                <h1 className="text-black font-weight-bold" ref={(el) => (this.headlineElementFirst = el)}>
                   Hi,
                 </h1>
-                <h1 className="text-black font-weight-bold" ref={this.headlineSecond}>
+                <h1 className="text-black font-weight-bold" ref={(el) => (this.headlineElementSecond = el)}>
                   I'm Radim Smílek,
                 </h1>
-                <h1 className="text-black font-weight-bold" ref={this.headlineThird}>
+                <h1 className="text-black font-weight-bold" ref={(el) => (this.headlineElementThird = el)}>
                   software developer.
                 </h1>
               </div>
-              <hr className="divider my-4" ref={this.divider} />
+              <div ref={(el) => (this.dividerElement = el)}>
+                <hr className="divider my-4" />
+              </div>
             </div>
             <div className="col-lg-8 align-self-baseline">
-              <h4 className="text-black font-weight-bold mb-5" ref={this.description}>
+              <h4 className="text-black font-weight-bold mb-5" ref={(el) => (this.descriptionElement = el)}>
                 Desktop &bull; Web &bull; Mobile
               </h4>
               <a className="button -primary" href="#skills">
