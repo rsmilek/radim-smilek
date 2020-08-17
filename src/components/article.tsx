6d06890499ccaf6390891709baa10eeb0d0f3bb4
rsmilek <smilek.radim@gmail.com>
2020-08-17 12:04:09 +0200

    + added props for image and children

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -3,18 +3,21 @@ import React, { FunctionComponent } from "react";
 type ArticleProps = {
   title: string;
   paragraphs: string[];
+  image: JSX.Element;
 };
 
 // Displays resume's article
-const Article: FunctionComponent<ArticleProps> = ({ title, paragraphs }) => {
+const Article: FunctionComponent<ArticleProps> = ({ title, paragraphs, image, children }) => {
   return (
     <React.Fragment>
       <div className="article-title inline">{title}</div>
+      {image}
       {paragraphs.map((paragraph, index) => (
-        <div key={index} className={"article-paragraph " + (index === 0 ? "inline" : "")}>
+        <div key={index} className={"article-paragraph " + (index === 0 ? "inline" : "mt-1")}>
           {paragraph}
         </div>
       ))}
+      {children}
     </React.Fragment>
   );
 };
